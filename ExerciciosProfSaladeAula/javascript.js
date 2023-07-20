f82c25df5c2863c92c8359f8cdd058f126f39ba8
mclmendonca <mfsm02@gmail.com>
2023-07-19 21:29:52 -0300

    update

---
+++ ExerciciosProfSaladeAula/javascript.js
@@ -0,0 +1,37 @@
+/*
+//1) Faça um algoritmo que solicite do usuário três valores e calcule a média das notas de um aluno;
+
+var nota1 = prompt('Digite a primeira nota: ')
+var nota2 = prompt('Digite a segunda nota: ')
+var nota3 = prompt('Digite a terceira nota: ')
+
+var media = (parseInt(nota1)+parseInt(nota2)+parseInt(nota3))/3
+
+alert(media)
+
+*/
+
+/*
+//2) Faça um algoritmo que solicite do usuário três valores e calcule a média das notas de um aluno:
+//Imprima as três notas (Ex. A primeira nota foi 8, a segunda foi ....)
+//Imprima a média (Ex. A média do aluno é 7)
+
+var nota1 = prompt('Digite a primeira nota: ')
+var nota2 = prompt('Digite a segunda nota: ')
+var nota3 = prompt('Digite a terceira nota: ')
+
+var media = (parseInt(nota1)+parseInt(nota2)+parseInt(nota3))/3
+
+alert('A primeira nota foi '+nota1+', a segunda foi '+nota2+', a terceira foi '+nota3+'. Logo a média é: '+media+'.')
+
+*/
+
+
+//⦁	Faça um algoritmo para que solicite o nome do usuário, a nota dele em duas avaliações e imprima o nome dele junto com a média de suas notas;
+
+var nome = prompt('Qual o seu nome? ')
+var nota1 = prompt('Digite a primeira nota: ')
+var nota2 = prompt('Digite a segunda nota: ')
+
+var media = (parseInt(nota1) + parseInt(nota2)) / 2
+
