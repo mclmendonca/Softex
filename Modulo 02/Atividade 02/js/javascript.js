22a6bf75f829c25b4745d82d029da2f84780b0ef
mclmendonca <mfsm02@gmail.com>
2023-08-08 00:29:49 -0300

    ups

---
+++ Modulo 02/Atividade 02/js/javascript.js
@@ -0,0 +1,19 @@
+
+
+
+var candidato1 = {nome: 'Luisinho', voto:0}
+var candidato2 = {nome: 'Jairinho', voto:0}
+var candidato3 = {nome: 'Moninha', voto:0}
+var branco = {nome: 'Branco', voto: 0}
+var nulo = {nome: 'Nulo', voto:0}
+
+
+var voto = parseInt(prompt('======== ||| Eleições ||| ========\rDigite 1 para votar em '+candidato1.nome+'\rDigite 2 para votar em '+candidato2.nome+'\rDigite 3 para votar em '+candidato3.nome+'\rDigite 0 para votar em '+branco.nome))
+
+
+
+
+
+
+
+
