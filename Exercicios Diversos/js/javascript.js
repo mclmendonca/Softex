496463fc75178ea8d64a24dab850959383f26015
mclmendonca <mfsm02@gmail.com>
2023-08-14 10:56:20 -0300

    tyu

--- Exercicios Diversos/js/javascript.js
+++ Exercicios Diversos/js/javascript.js
@@ -1,3 +1,4 @@
+/*
 let matriz = [
     [1,2,3],
     [4,5,6],
@@ -9,3 +10,32 @@ matriz.forEach(row => {
         console.log(element)
 })
 })
+*/
+try {
+    
+let media = 0
+let notas = [
+
+    [5.5, 7.9, 7.1],  //Notas de José
+    [7, 3.5, 9],      //Notas de Lúcia
+    [8.6, 7.3, 8.8]   //Notas de Pedro
+
+]
+
+for (let i = 0; i < notas.length; i++) {
+    for (let j = 0; j < notas.length; j++) {
+        media = (media + notas[i][j])
+       
+
+
+    }
+
+    notas.push(media)
+}
+
+console.log(notas)
+
+} catch (error) {
+    console.log(Error.message)
+}
+
