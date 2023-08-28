8488847dab34aed59867f23f387481f1237ef55b
mclmendonca <mfsm02@gmail.com>
2023-08-27 23:44:53 -0300

    update

---
+++ PARTE 2/Modulo 1/Exercicio 1/js/javascript.js
@@ -0,0 +1,31 @@
+
+
+var booleano
+var bigInt
+var string
+var symbol
+
+console.log(booleano)
+console.log(bigInt)
+console.log(string)
+console.log(symbol)
+
+booleano = null
+bigInt = null
+string = null
+symbol = null
+
+console.log(booleano)
+console.log(bigInt)
+console.log(string)
+console.log(symbol)
+
+booleano = true
+bigInt = 9007199254740991
+string = "Olá Softex"
+symbol = Symbol()
+
+console.log(booleano)
+console.log(bigInt)
+console.log(string)
+console.log(symbol)
