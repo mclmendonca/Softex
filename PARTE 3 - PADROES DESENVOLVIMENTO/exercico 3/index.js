f3793f89d4d89af020ee041203213e74425adcb3
mclmendonca <mfsm02@gmail.com>
2023-11-19 20:54:33 -0300

    Create index.js

---
+++ PARTE 3 - PADROES DESENVOLVIMENTO/exercico 3/index.js
@@ -0,0 +1,42 @@
+// Singleton SistemaSeguranca
+var SistemaSeguranca = (function () {
+  var instancia;
+
+  function SistemaSeguranca() {
+    // A senha da base secreta
+    this.senhaBaseSecreta = "senha123";
+  }
+
+  SistemaSeguranca.prototype.acessarBaseSecreta = function (senha) {
+    if (senha === this.senhaBaseSecreta) {
+      console.log("Acesso concedido à base secreta!");
+    } else {
+      console.log("Acesso negado! Senha incorreta.");
+    }
+  };
+
+  return {
+    getInstance: function () {
+      if (!instancia) {
+        instancia = new SistemaSeguranca();
+      }
+      return instancia;
+    },
+  };
+})();
+
+// Programa principal
+var agenteSecreto = SistemaSeguranca.getInstance();
+
+
+
+
+console.log('======= MI7 System =======')
+console.log('====== Base Secreta ======')
+console.log('Digite a senha de acesso: ')
+
+// Tentativa de acesso à base secreta
+agenteSecreto.acessarBaseSecreta("senha123"); // Acesso concedido à base secreta!
+
+// Tentativa de acesso com senha incorreta
+agenteSecreto.acessarBaseSecreta("senha456"); // Acesso negado! Senha incorreta.
