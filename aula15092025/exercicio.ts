129636391a78c4b8a38327ec8b2e145e6d17ee59
mclmendonca <mfsm02@gmail.com>
2025-09-15 20:29:33 -0300

    exercicio pra entregar

---
+++ aula15092025/exercicio.ts
@@ -0,0 +1,92 @@
+// Welcome to the TypeScript Playground, this is a website
+// which gives you a chance to write, share and learn TypeScript.
+
+// You could think of it in three ways:
+//
+//  - A location to learn TypeScript where nothing can break
+//  - A place to experiment with TypeScript syntax, and share the URLs with others
+//  - A sandbox to experiment with different compiler features of TypeScript
+
+// const anExampleVariable = "Hello World"
+// console.log(anExampleVariable)
+
+// To learn more about the language, click above in "Examples" or "What's New".
+// Otherwise, get started by removing these comments and the world is your playground.
+
+/*
+// 1) Cadastro simples
+// Crie variáveis para guardar: nome, idade, cidade e se a pessoa estuda. Mostre no console uma frase com essas informações.
+
+let nome: string = "Marcelo"
+let idade: number = 23
+let cidade: string = "Recife"
+let estuda: boolean = true
+console.log(`Seu nome é: ${nome}\n Sua idade é: ${idade}\n Sua cidade é:${cidade}\n Estuda? : ${estuda}`)
+*/
+
+/*
+// 2) Área do retângulo
+// Peça largura e altura (variáveis fixas). Calcule a área (largura * altura).
+
+let largura = Number(prompt("Qual a largura? "))
+let altura  = Number(prompt("Qual a altura? "))
+let area = largura * altura
+alert("A área do retângulo é: "+area)
+*/
+
+/*
+// 3) Área do círculo
+// Guarde o valor do raio em uma variável e calcule a área usando π * raio².
+
+let raio = Number(prompt("Qual o valor do Raio? "))
+
+let areadoraio = (raio**2)*3.14
+
+alert(`A Área do Raio é: ${areadoraio}`)
+*/
+
+/*
+// 4) Conversor de temperatura
+// Guarde uma temperatura em Celsius e calcule em Fahrenheit.
+// Fórmula: F = C * 1.8 + 32.
+
+let celcius = Number(prompt("Qual a temperatura em ºC? "))
+let fare = (celcius*1.8)+32
+console.log(`${celcius} ºC em Fahrenheit é: ${fare} ºF`)
+*/
+
+/*
+// 5) Antecessor e sucessor
+// Dado um número, mostre qual é o antecessor e o sucessor.
+
+let numero = Number(prompt("Qual o número?"))
+console.log(`O anterior do seu numero ${numero} é: ${numero-1} e o posterior é: ${numero+1}. Diga Feito !`)
+*/
+
+/*
+// 6) Dobro e triplo
+// Dado um número, mostre o dobro, triplo e a raiz quadrada.
+
+let numero = Number(prompt("Qual o número?"))
+console.log(`0 dobro do seu numero ${numero} é: ${numero*2}. e o triplo é: ${numero*3} e a raiz quadrada é: ${numero**0.5}.`)
+*/
+
+/*
+// 7) Média de notas
+// Crie três variáveis com notas de um aluno e calcule a média.
+
+let numeros =[]
+let soma = 0
+let qtd = 0
+
+for(let i = 0;i<3;i++){
+    numeros[i] = Number(prompt(`Digite a ${i+1}ª nota: `));
+    soma += numeros[i];
+    qtd++
+}
+
+console.log(`A média das notas é: ${soma/qtd}`)
+*/
+
+
+
